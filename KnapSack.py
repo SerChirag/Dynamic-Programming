73429a17037ad48753eedc37dc7c9d5fc9fa93b9
Chirag Vashist <you@example.com>
2018-01-02 10:07:03 +0300

    KnapSack Efficient Implementation

--- KnapSack.py
+++ KnapSack.py
@@ -1,5 +1,29 @@
 def KnapSack(W,n):
 
+    K = [[0 for i in range(W+1)] for j in range(n+1)]
+    for i in range(n+1):
+        for j in range(W+1):
+            if(i==0 or j==0):
+                K[i][j] = 0
+            elif(weight[i-1] <= j):
+                K[i][j] = max(K[i-1][j], value[i-1] + K[i-1][j-weight[i-1]])
+                
+            else:
+
+                K[i][j] = K[i-1][j]
+
+    for i in range(n+1):
+        for j in range(W+1):
+            print K[i][j],
+        print
+
+    return K[n][W]
+
+    
+
+
+def KnapSackNaive(W,n):
+
     if(n==0 or W==0):
         return 0
     
@@ -9,7 +33,6 @@ def KnapSack(W,n):
     else:
         return max(KnapSack(W,n-1), value[n-1] + KnapSack(W-weight[n-1],n-1))
         
-    
     
 
 value = [60,100,120]
