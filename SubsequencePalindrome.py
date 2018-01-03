13177ab6eb87286a253efbd70ab20b8a28018f3a
Chirag Vashist <you@example.com>
2018-01-03 16:28:12 +0300

    Longest Palindromic Subsequence Complete

--- SubsequencePalindrome.py
+++ SubsequencePalindrome.py
@@ -1,36 +1,26 @@
 def SP(s):
 
-    l = [[0 for j in range(s+1)] for i in range(s+1)]
-    for i in range(s):
-        for j in range(s):
+    p = len(s)
+    e = [[0 for j in range(p)] for i in range(p)]
 
-            if(i == j):
-                l[i][j] = 1
+    for l in range(1,p+1):
+        for i in range(0,p-l+1):
 
-            elif(i == j-1 and s[i] == s[j]):
-                l[i][j] = 2
-
-            elif(s[i] == s[j]):
-                l[i][j] = 2 + l[i+1][j-1]
-
+            j = i+l-1
             
-                
-
-    global s
-    if(i == j):
-        return 1
-    
-    if(i == j-1 and s[i] == s[j]):
+            if(i == j):
+                e[i][j] = 1
+            elif(i == j-1 and s[i] == s[j]):
+                e[i][j] = 2
+            elif(s[i] == s[j]):
+                e[i][j] = 2 + e[i+1][j-1]
+            else:
+                e[i][j] = max(e[i][j-1],e[i+1][j])
         
-        return 2
-
-    if(s[i] == s[j]):
-        
-        return (2 + SPN(i+1,j-1))
 
 
-    
-    return max(SPN(i,j-1),SPN(i+1,j))
+  
+    return e[0][p-1]
 
 
 def SPN(i,j):
@@ -51,7 +41,7 @@ def SPN(i,j):
 
 
 
-s = "geeks"
+s = "GEEKSFORGEEKS"
 a = len(s)-1
 #print SPN(0,a)
-print SP(a)
+print SP(s)
