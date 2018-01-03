b36ad08e186b3af1fbb0a4e7952c00d108112acd
Chirag Vashist <you@example.com>
2018-01-03 15:49:49 +0300

    Matrix Multiplication Dynamic Version

--- MatricChainMult.py
+++ MatricChainMult.py
@@ -1,4 +1,25 @@
-def mult(i,j):
+def mult(n):
+
+    edit = [[0 for j in range(n+1)] for i in range(n+1)]
+
+    for l in range(2,n):
+        for i in range(1,n-l+1):
+
+            j = i+l-1
+            m = float('inf')
+
+            for k in range(i,j):
+
+                count = edit[i][k] + edit[k+1][j] + a[i-1]*a[k]*a[j]
+                if(count < m):
+                    m = count
+
+            edit[i][j] = m
+
+    return edit[1][n-1]
+
+    
+def multN(i,j):
 
     if(i == j):
         return 0
@@ -15,13 +36,7 @@ def mult(i,j):
     return m
     
 
-    
-
-
-
-
-
 
 a = [40,20,30,10,30]
-n = len(a)-1
-print mult(1,n)
+n = len(a)
+print mult(n)
